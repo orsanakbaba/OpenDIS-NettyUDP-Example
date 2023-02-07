98252fb064fcc5f7b6913495e7a3e0f412e333fb
orsanakbaba <orsanak@gmail.com>
2023-02-07 23:10:52 +0300

    initial commit

---
+++ src/main/java/tr/com/orsan/academy/learning/netty/opendis/publisher/Publisher.java
@@ -0,0 +1,65 @@
+package tr.com.orsan.academy.learning.netty.opendis.publisher;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Publisher {
+    private static final Logger logger = LogManager.getLogger(Publisher.class);
+    private final int port;
+
+    public Publisher(int port) {
+        this.port = port;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 51444;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        new Publisher(port).run();
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            //ch.pipeline().addLast(new SimpleNettySbeDecoder());
+                        }
+                    })
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+
+            ChannelFuture f = b.bind("127.0.0.1", this.port).sync(); // (7)
+            logger.debug("Netty Server started.");
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
